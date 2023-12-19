5cf24a0e123300167ca413d956e0cb519cff896b
Desiree Pelletier <pelletier.desiree@gmail.com>
2023-12-18 20:42:09 -0500

    changing repo structure and adding .gitignore

--- parse_csv.sh
+++ parse_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-csv_file="./arrays_table.csv"
+csv_file="./test-data/arrays_table.csv"
 
 # parse table columns into 3 arrays
 column_0=($(cut -d "," -f 1 $csv_file))
@@ -14,9 +14,11 @@ echo "${column_0[@]}"
 ## Create a new array as the difference of columns 1 and 2
 # initialize array with header
 column_3=("column_3")
+
 # get the number of lines in each column
 nlines=$(cat $csv_file | wc -l)
 echo "There are $nlines lines in the file"
+
 # populate the array
 for ((i=1; i<$nlines; i++)); do
   column_3[$i]=$((column_2[$i] - column_1[$i]))
